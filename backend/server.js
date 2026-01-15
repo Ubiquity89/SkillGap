beb235dbf2acccb73eeb31bbe0ded64fb95d98ea
Ubiquity89 <ubiquity89@gmail.com>
2026-01-15 14:36:04 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -26,13 +26,10 @@ app.use(
       callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-// THIS IS IMPORTANT FOR PREFLIGHT
-app.options("*", cors());
 
 /* =====================================================
    BODY PARSERS
